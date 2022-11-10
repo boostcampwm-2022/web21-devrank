0b8e11c62d44a5b6bbe4217442fb0e750225e6ae
wkddntjr1123 <hbo1123@naver.com>
2022-11-10 18:51:47 +0900

    chore : eslint 설정 - import문 정렬

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -2,13 +2,48 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  // need dependency : 'eslint-plugin-simple-import-sort'
+  plugins: [
+    'prettier',
+    '@typescript-eslint/eslint-plugin',
+    'simple-import-sort',
+  ],
   extends: [
+    'prettier',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      files: ['**/*.js', '**/*.ts', '**/*.tsx'],
+      rules: {
+        'max-len': ['error', { code: 120 }],
+        'simple-import-sort/imports': [
+          'error',
+          {
+            // 1. start with (@ or ~)
+            // 2. start with (alphabet)
+            // 3. start with (../)
+            // 4. start with (./)
+            groups: [
+              [
+                '^@',
+                '^~',
+                '^[a-z]',
+                '^\\.\\.(?!/?$)',
+                '^\\.\\./?$',
+                '^\\./(?=.*/)(?!/?$)',
+                '^\\.(?!/?$)',
+                '^\\./?$',
+              ],
+            ],
+          },
+        ],
+      },
+    },
   ],
   root: true,
   env: {
@@ -17,9 +52,12 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'prettier/prettier': 'error',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
   },
 };
