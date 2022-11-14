51140339959303dfa97d4daf776f9eccb9db7281
asdf99245 <asdf99245@naver.com>
2022-11-14 23:03:11 +0900

    chore: next svg 사용 설정

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
     styledComponents: true, // styled-components 적용
     removeConsole: process.env.NODE_ENV === 'production', // 배포 환경일시 console.* 제거
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
   images: {
     remotePatterns: [
       // 외부 이미지 허용
