6a61fc95ef1d88140b7940985d84f69cf624150c
asdf99245 <asdf99245@naver.com>
2022-11-15 01:35:04 +0900

    chore: storybook 절대경로 설정

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,15 +1,24 @@
+const path = require('path');
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-addon-styled-component-theme/dist/preset',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  webpackFinal: async (config) => {
+    config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules', 'src/styles'];
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../src'),
+    };
+
+    return config;
+  },
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+};
