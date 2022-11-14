f12f9ba3c5978ee9287879261baffdfe795a1f77
asdf99245 <asdf99245@naver.com>
2022-11-15 01:35:19 +0900

    chore: storybook styled-components 전역, 공통 스타일 적용

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,9 +1,22 @@
+import GlobalStyles from '../src/styles/globalStyles';
+import theme from '../src/styles/theme';
+import { ThemeProvider } from 'styled-components';
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Story />
+    </ThemeProvider>
+  ),
+];
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
