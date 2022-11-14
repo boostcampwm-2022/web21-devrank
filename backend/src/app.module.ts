79d312ad9083e74ac5fb64f43b98bf508e0fcfd2
Sion Kang <31057849+Yaminyam@users.noreply.github.com>
2022-11-14 15:23:20 +0900

    fix: 잘못된 경로에 설치된 의존성 이전

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), MongooseModule.forRoot('mongodb://localhost/nest')],
