836f3a4953077286800712e3059a35d5fbf5398b
asdf99245 <asdf99245@naver.com>
2022-11-14 21:47:24 +0900

    chore: next 외부이미지 허용 설정
    Co-authored-by: tunggary <tunggary@users.noreply.github.com>

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,16 @@ const nextConfig = {
   swcMinify: true,
   compiler: {
     styledComponents: true, // styled-components 적용
-    removeConsole: process.env.NODE_ENV === 'production' ? true : false, // 배포 환경일시 console.* 제거
+    removeConsole: process.env.NODE_ENV === 'production', // 배포 환경일시 console.* 제거
+  },
+  images: {
+    remotePatterns: [
+      // 외부 이미지 허용
+      {
+        protocol: 'https',
+        hostname: 'cdn.jsdelivr.net',
+      },
+    ],
   },
 };
 
