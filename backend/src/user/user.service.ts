ae810c5e437920bde4d438a4c542cb3d25f2a48b
Sion Kang <31057849+Yaminyam@users.noreply.github.com>
2022-11-15 23:20:27 +0900

    refactor: get을 find로 변경
    Co-Authored-By: 장우석 <64186072+wkddntjr1123@users.noreply.github.com>

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,27 +1,32 @@
 import { NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { UserRepository } from './user.repository';
 import { User } from './user.schema';
 
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
 
-  async getAll(): Promise<any> {
-    return this.userRepository.getAll();
+  async findAll(): Promise<User[]> {
+    return this.userRepository.findAll();
   }
 
-  async getById(id: string): Promise<any> {
-    const user = await this.userRepository.getById(id);
+  async findOneByGithubId(id: number): Promise<User> {
+    const user = await this.userRepository.findOneByGithubId(id);
     if (!user) {
       throw new NotFoundException('User not found');
     }
     return user;
   }
 
-  async create(user: User): Promise<any> {
+  async create(user: User): Promise<User> {
     return this.userRepository.create(user);
   }
 
-  async update(id: string, user: User): Promise<any> {
+  async update(id: number, user: User): Promise<User> {
+    return this.userRepository.update(id, user);
+  }
+
     return this.userRepository.update(id, user);
   }
 }
