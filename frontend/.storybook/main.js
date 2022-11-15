844083eb5eeeed432088a5728c06bf680d0b1c0d
asdf99245 <asdf99245@naver.com>
2022-11-15 22:16:38 +0900

    chore: storybook svg 사용 설정

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -16,6 +16,10 @@ module.exports = {
     'storybook-addon-styled-component-theme/dist/preset',
   ],
   webpackFinal: async (config) => {
+    config.module.rules.unshift({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
     config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules', 'src/styles'];
     config.resolve.alias = {
       ...config.resolve.alias,
