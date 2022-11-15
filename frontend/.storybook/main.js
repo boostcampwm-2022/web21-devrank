1fe04fa142daded02a8f94a00d0353c5151d6b8e
asdf99245 <asdf99245@naver.com>
2022-11-15 18:12:56 +0900

    chore: storybook path alias 설정 수정

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -19,7 +19,13 @@ module.exports = {
     config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules', 'src/styles'];
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve(__dirname, '../src'),
+      '@components': path.resolve(__dirname, '../src/components/'),
+      '@styles': path.resolve(__dirname, '../src/styles/'),
+      '@utils': path.resolve(__dirname, '../src/utils/'),
+      '@apis': path.resolve(__dirname, '../src/apis/'),
+      '@hooks': path.resolve(__dirname, '../src/hooks/'),
+      '@types': path.resolve(__dirname, '../src/types/'),
+      '@contexts': path.resolve(__dirname, '../src/contexts/'),
     };
 
     return config;
