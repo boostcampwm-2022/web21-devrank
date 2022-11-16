bca82a839233b835cc7fdd1572c7ff48233a0b1e
tunggary <gsy1656@gmail.com>
2022-11-16 21:29:54 +0900

    feat: avatar 컴포넌트 추가

--- frontend/src/components/common/index.ts
+++ frontend/src/components/common/index.ts
@@ -1,4 +1,6 @@
-import Button from '@components/common/Button';
-import LanguageIcon from '@components/common/LanguageIcon';
+import Button from './Button';
+import LanguageIcon from './LanguageIcon';
+import { Dropdown ,DropdownItem} from './Dropdown';
+import Avatar from './Avatar';
 
-export { LanguageIcon, Button };
+export { LanguageIcon, Button, Dropdown, DropdownItem, Avatar };
