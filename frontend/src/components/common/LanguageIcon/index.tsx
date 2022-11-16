e031f7cca8f7345bc27597ff98abb38f8877cea6
tunggary <gsy1656@gmail.com>
2022-11-16 21:39:49 +0900

    rename: LanguageIcon 컴포넌트 폴더 변경

---
+++ frontend/src/components/common/LanguageIcon/index.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+import { LanguageIconProps } from '@types';
+import { languageToURL } from '@utils/utils';
+
+function LanguageIcon({ language, width = 50, height = 50 }: LanguageIconProps) {
+  const iconImgSrc = languageToURL(language.toLocaleLowerCase());
+  return <Image src={iconImgSrc} alt={`${language}`} width={width} height={height} quality={100} />;
+}
+
+export default LanguageIcon;
