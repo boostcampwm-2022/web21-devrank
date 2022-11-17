81c4538a39cc31217570759adacd722565e4469c
asdf99245 <asdf99245@naver.com>
2022-11-17 16:39:37 +0900

    feat: 로그인 client access token 등록 및 token refresh 로직 구현
    - axios interceptor를 통해 응답시 access token 상태로 관리
    - api요청시 토큰 만료일 시 refresh api 요청

--- frontend/src/utils/axiosInstance.ts
+++ frontend/src/utils/axiosInstance.ts
@@ -7,4 +7,33 @@ const instance = axios.create({
   withCredentials: true,
 });
 
+instance.interceptors.response.use(
+  (res) => {
+    // 응답와서 access token 있으면 Authorization Header에 저장
+    const accessToken = res.data.accessToken;
+    if (accessToken) {
+      axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+    }
+
+    return res;
+  },
+  async (err) => {
+    const originConfig = err.config;
+
+    // 요청했는데 token 만료면 token refresh 요청
+    if (originConfig.url !== '/api/auth/login' && err.response) {
+      if (err.response.status === 401 && err.response.retry) {
+        try {
+          const { data } = await instance.post('/api/auth/refresh');
+          axios.defaults.headers.common['Authorization'] = `Bearer ${data.accessToken}`;
+
+          return instance(originConfig);
+        } catch (_err) {
+          return Promise.reject(_err);
+        }
+      }
+    }
+  },
+);
+
 export default instance;
