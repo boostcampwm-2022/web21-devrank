ce5544170b6df3dcbbbabf27682efede61888282
asdf99245 <asdf99245@naver.com>
2022-11-17 22:51:47 +0900

    refactor: 로그인 동작 tanstack-query 방법으로 변경

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,13 +1,11 @@
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { Noto_Sans_KR } from '@next/font/google';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Layout from '@components/Layout';
-import { AuthContext, AuthType } from '@contexts/authContext';
 import GlobalStyles from '@styles/globalStyles';
 import theme from '@styles/theme';
 
@@ -17,22 +15,6 @@ const notoSansKR = Noto_Sans_KR({
 
 const queryClient = new QueryClient();
 
-interface AuthProviderProps {
-  children: React.ReactNode;
-}
-
-function AuthProvider({ children }: AuthProviderProps) {
-  const [auth, setAuth] = useState<AuthType>({
-    isLoggedIn: false,
-    user: {
-      id: '',
-      username: '',
-      avatarUrl: '',
-    },
-  });
-  return <AuthContext.Provider value={{ auth, setAuth }}>{children}</AuthContext.Provider>;
-}
-
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
@@ -41,14 +23,12 @@ function App({ Component, pageProps }: AppProps) {
       </Head>
       <QueryClientProvider client={queryClient}>
         <ThemeProvider theme={theme}>
-          <AuthProvider>
-            <GlobalStyles />
-            <div className={notoSansKR.className}>
-              <Layout>
-                <Component {...pageProps} />
-              </Layout>
-            </div>
-          </AuthProvider>
+          <GlobalStyles />
+          <div className={notoSansKR.className}>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </div>
         </ThemeProvider>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
