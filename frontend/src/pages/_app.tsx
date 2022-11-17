e3986d52fa70fdf555bab8358a014e878b4a2537
asdf99245 <asdf99245@naver.com>
2022-11-17 14:33:02 +0900

    feat: tanstack-query 적용

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,32 +3,38 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
 import { Noto_Sans_KR } from '@next/font/google';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Layout from '@components/Layout';
+import { AuthContext } from '@contexts/authContext';
 import GlobalStyles from '@styles/globalStyles';
 import theme from '@styles/theme';
-import { AuthContext } from '@contexts/authContext';
 
 const notoSansKR = Noto_Sans_KR({
   weight: ['100', '300', '400', '500', '700', '900'],
 });
 
-function App({ Component, pageProps }: AppProps) {
+const queryClient = new QueryClient();
 
+function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Devrank</title>
       </Head>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <AuthContext.Provider value={false}>
-          <div className={notoSansKR.className}>
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          </div>
-        </AuthContext.Provider>
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <AuthContext.Provider value={false}>
+            <GlobalStyles />
+            <div className={notoSansKR.className}>
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
+            </div>
+          </AuthContext.Provider>
+        </ThemeProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   );
 }
