1148c4285697bde291fec2f771a03eb53d7f4ad1
tunggary <gsy1656@gmail.com>
2022-11-19 19:24:48 +0900

    feat: Avatar 컴포넌트 이름 필드 추가

--- frontend/src/components/common/Avatar/index.tsx
+++ frontend/src/components/common/Avatar/index.tsx
@@ -7,6 +7,7 @@ type AvatarSize = 'sm' | 'md' | 'lg';
 interface AvatarProps {
   src: string;
   size?: AvatarSize;
+  name?: string;
   onClick?: (e: ClickEvent) => void;
 }
 
@@ -14,17 +15,29 @@ interface StyledContainerProps {
   size: AvatarSize;
 }
 
-function Avatar({ src, size = 'md', onClick }: AvatarProps) {
+function Avatar({ src, size = 'md', name, onClick }: AvatarProps) {
   return (
-    <Container size={size} onClick={onClick}>
-      <Image src={src} alt='프로필 이미지' fill />
+    <Container>
+      <ImageContainer size={size} onClick={onClick}>
+        <Image src={src} alt='프로필 이미지' fill />
+        {name}
+      </ImageContainer>
+      {name}
     </Container>
   );
 }
 
 export default Avatar;
 
-const Container = styled.div<StyledContainerProps>`
+const Container = styled.div`
+  width: max-content;
+  ${({ theme }) => theme.common.flexCenter};
+  gap: 12px;
+  font-size: ${({ theme }) => theme.fontSize.lg};
+  padding-right: 12px;
+`;
+
+const ImageContainer = styled.div<StyledContainerProps>`
   ${({ theme }) => theme.common.flexCenter};
   border-radius: 50%;
   width: 58px;
