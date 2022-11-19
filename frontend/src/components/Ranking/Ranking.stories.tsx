fb94349a21ee390fe53d26e038e41fd9fc4724ca
asdf99245 <asdf99245@naver.com>
2022-11-20 02:34:49 +0900

    fix: Storybook에서 랭킹 테이블 로우에 스타일이 적용되지 않던 현상 수정

--- frontend/src/components/Ranking/Ranking.stories.tsx
+++ frontend/src/components/Ranking/Ranking.stories.tsx
@@ -7,41 +7,41 @@ import CubeIcon from '@components/common/CubeIcon';
 export default {
   title: 'Components/Ranking',
   component: Ranking,
+  subcomponents: { Avatar, CubeIcon },
 } as ComponentMeta<typeof Ranking>;
 
-const Template: ComponentStory<typeof Ranking> = (args) => <Ranking {...args} />;
-
 const GrayText = styled.span`
-  color: ${({ theme }) => theme.colors.gray6};
+  color: ${({ theme }) => theme.colors.gray5};
 `;
 
-export const Overall = Template.bind({});
+const OverallTemplate: ComponentStory<typeof Ranking> = (args) => (
+  <Ranking {...args}>
+    <Ranking.Head>
+      <Ranking.Element>티어</Ranking.Element>
+      <Ranking.Element>#</Ranking.Element>
+      <Ranking.Element>사용자</Ranking.Element>
+      <Ranking.Element>점수</Ranking.Element>
+    </Ranking.Head>
+    <Ranking.Row>
+      <Ranking.Element>
+        <CubeIcon tier={'red'} />
+      </Ranking.Element>
+      <Ranking.Element>
+        <GrayText>{10}</GrayText>
+      </Ranking.Element>
+      <Ranking.Element>
+        <Avatar src='/profile-dummy.png' name='tunggary' />
+      </Ranking.Element>
+      <Ranking.Element>
+        <GrayText>{(10000).toLocaleString()}</GrayText>
+      </Ranking.Element>
+    </Ranking.Row>
+  </Ranking>
+);
+
+export const Overall = OverallTemplate.bind({});
 Overall.args = {
   width: 512,
   columnWidthList: ['12%', '8%', '58%', '20%'],
   columnAlignList: ['left', 'left', 'left', 'right'],
-  children: (
-    <>
-      <Ranking.Head>
-        <Ranking.Element>티어</Ranking.Element>
-        <Ranking.Element>#</Ranking.Element>
-        <Ranking.Element>사용자</Ranking.Element>
-        <Ranking.Element>점수</Ranking.Element>
-      </Ranking.Head>
-      <Ranking.Row>
-        <Ranking.Element>
-          <CubeIcon tier={'red'} />
-        </Ranking.Element>
-        <Ranking.Element>
-          <GrayText>{10}</GrayText>
-        </Ranking.Element>
-        <Ranking.Element>
-          <Avatar src='/profile-dummy.png' name='tunggary' />
-        </Ranking.Element>
-        <Ranking.Element>
-          <GrayText>{(10000).toLocaleString()}</GrayText>
-        </Ranking.Element>
-      </Ranking.Row>
-    </>
-  ),
 };
