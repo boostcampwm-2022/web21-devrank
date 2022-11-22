ce6e6ce8eb0c7a213fdc58561369ade0a41932b7
tunggary <gsy1656@gmail.com>
2022-11-22 15:20:57 +0900

    chore: type index에서  common 으로 변경

--- frontend/src/type/index.ts
+++
@@ -1,7 +0,0 @@
-export type ButtonClickEvent = React.MouseEvent<HTMLButtonElement>;
-
-export type ClickEvent = React.MouseEvent<HTMLElement>;
-
-export type InputChangeEvent = React.ChangeEvent<HTMLInputElement>;
-
-export type FormEvent = React.FormEvent<HTMLFormElement>;
