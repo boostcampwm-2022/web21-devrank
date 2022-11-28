fad33f385d65e00eb5899e8c00273840f5857065
asdf99245 <asdf99245@naver.com>
2022-11-28 12:06:31 +0900

    refactor: useQueryData useMemo, useCallback 적용

--- frontend/src/hooks/useQueryData.ts
+++ frontend/src/hooks/useQueryData.ts
@@ -1,17 +1,22 @@
-import { useQueryClient } from '@tanstack/react-query';
+import { useCallback, useMemo } from 'react';
+import { useIsFetching, useQueryClient } from '@tanstack/react-query';
 
 function useQueryData(queryKey: unknown[]) {
   const queryClient = useQueryClient();
+  const isFetching = useIsFetching();
 
-  const queryData: any = queryClient.getQueryData(queryKey);
+  const queryData: any = useMemo(() => queryClient.getQueryData(queryKey), [isFetching, queryClient, queryKey]);
 
-  const removeQueryData = () => {
+  const removeQueryData = useCallback(() => {
     queryClient.removeQueries(queryKey);
-  };
+  }, [queryClient, queryKey]);
 
-  const setQueryData = (data: any) => {
-    queryClient.setQueryData(queryKey, data);
-  };
+  const setQueryData = useCallback(
+    (data: any) => {
+      queryClient.setQueryData(queryKey, data);
+    },
+    [queryClient, queryKey],
+  );
 
   return { queryData, setQueryData, removeQueryData };
 }
