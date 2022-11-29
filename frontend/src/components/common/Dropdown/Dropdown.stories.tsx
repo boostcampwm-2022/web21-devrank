ab99ddaf20f5b6d35106391be8e06d7d84e37d08
tunggary <gsy1656@gmail.com>
2022-11-29 21:56:21 +0900

    test: Dropdown 스토리북 추가
    Co-authored-by: asdf99245 <asdf99245@users.noreply.github.com>

--- frontend/src/components/common/Dropdown/Dropdown.stories.tsx
+++ frontend/src/components/common/Dropdown/Dropdown.stories.tsx
@@ -8,14 +8,6 @@ export default {
   component: Dropdown,
 } as ComponentMeta<typeof Dropdown>;
 
-const Template: ComponentStory<typeof Dropdown> = (args) => (
-  <Dropdown {...args}>
-    <Dropdown.Item>item1</Dropdown.Item>
-    <Dropdown.Item>item2</Dropdown.Item>
-    <Dropdown.Item>item3</Dropdown.Item>
-  </Dropdown>
-);
-
 const LanguageButton = styled(Button)`
   font-size: ${({ theme }) => theme.fontSize.md};
   background-color: transparent;
@@ -29,12 +21,18 @@ const LanguageButton = styled(Button)`
   }
 `;
 
-export const LanguageButtonDropdown = Template.bind({});
-LanguageButtonDropdown.args = {
-  trigger: (
-    <LanguageButton size='md'>
-      <Image src='/icons/globe.svg' alt='언어 선택 버튼' width={25} height={25} />
-      <span>안녕 난 트리거</span>
-    </LanguageButton>
-  ),
-};
+const Template: ComponentStory<typeof Dropdown> = (args) => (
+  <Dropdown {...args}>
+    <Dropdown.Trigger>
+      <LanguageButton size='md'>
+        <Image src='/icons/globe.svg' alt='언어 선택 버튼' width={25} height={25} />
+        <span>안녕 난 트리거</span>
+      </LanguageButton>
+    </Dropdown.Trigger>
+    <Dropdown.Item>item1</Dropdown.Item>
+    <Dropdown.Item>item2</Dropdown.Item>
+    <Dropdown.Item>item3</Dropdown.Item>
+  </Dropdown>
+);
+
+export const Basic = Template.bind({});
