36b7e9c5ca5c9fefd6e9923bc2374a8f3d7faaf2
장우석 <wkddntjr1123@gmail.com>
2022-11-30 22:40:42 +0900

    feat: user 스키마에 history, pinnedRepositories 추가

--- backend/src/user/dto/user.dto.ts
+++ backend/src/user/dto/user.dto.ts
@@ -1,24 +1,71 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { History } from './history.dto';
+import { PinnedRepositoryDto } from './pinned-repository.dto';
+
 export class UserDto {
-  _id?: string;
+  @ApiProperty()
   id: string;
+
+  @ApiProperty()
   username: string;
+
+  @ApiProperty()
   following: number;
+
+  @ApiProperty()
   followers: number;
+
+  @ApiProperty()
   commitsScore?: number;
+
+  @ApiProperty()
   issuesScore?: number;
+
+  @ApiProperty()
   followersScore?: number;
+
+  @ApiProperty()
   score?: number;
+
+  @ApiProperty()
   tier?: string;
+
+  @ApiProperty()
   avatarUrl?: string;
+
+  @ApiProperty()
   name?: string;
+
+  @ApiProperty()
   company?: string;
+
+  @ApiProperty()
   blogUrl?: string;
+
+  @ApiProperty()
   location?: string;
+
+  @ApiProperty()
   bio?: string;
+
+  @ApiProperty()
   email?: string;
+
+  @ApiProperty()
   dailyViews?: number;
+
+  @ApiProperty()
   scoreDifference?: number;
+
+  @ApiProperty()
   updateDelayTime?: number;
-  repositories?: number[];
+
+  @ApiProperty()
   primaryLanguages?: string[];
+
+  @ApiProperty({ type: History })
+  history?: History;
+
+  @ApiProperty({ isArray: true, type: PinnedRepositoryDto })
+  pinnedRepositories?: PinnedRepositoryDto[];
 }
