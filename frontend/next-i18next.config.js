13e63580c39cad1c2e0e45d7c42392302cfd0b74
tunggary <gsy1656@gmail.com>
2022-11-30 15:05:47 +0900

    fix: add localePath

--- frontend/next-i18next.config.js
+++ frontend/next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     // 어플리케이션에서 지원할 언어 리스트
     locales: ['en', 'ko'],
     defaultLocale: 'ko',
   },
+  localePath: path.resolve('./public/locales'),
 };
