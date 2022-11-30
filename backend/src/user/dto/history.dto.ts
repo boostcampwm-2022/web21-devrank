c9cea3c777a5779fd2e37cdc0448233406a87122
장우석 <wkddntjr1123@gmail.com>
2022-11-30 22:40:00 +0900

    feat: 프로필에서 stat정보를 나타내는 History Dto 추가

---
+++ backend/src/user/dto/history.dto.ts
@@ -0,0 +1,46 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+class DailyInfo {
+  @ApiProperty()
+  count: number;
+
+  @ApiProperty()
+  level: number;
+}
+
+class DailyItem {
+  @ApiProperty()
+  dateTime: DailyInfo;
+}
+
+export class History {
+  @ApiProperty()
+  totalCommitContributions: number;
+
+  @ApiProperty()
+  totalIssueContributions: number;
+
+  @ApiProperty()
+  totalPullRequestContributions: number;
+
+  @ApiProperty()
+  totalPullRequestReviewContributions: number;
+
+  @ApiProperty()
+  totalRepositoryContributions: number;
+
+  @ApiProperty()
+  stargazerCount: number;
+
+  @ApiProperty()
+  forkCount: number;
+
+  @ApiProperty()
+  maxContinuosCount: number;
+
+  @ApiProperty({
+    isArray: true,
+    type: DailyItem,
+  })
+  contributionHistory: DailyItem;
+}
