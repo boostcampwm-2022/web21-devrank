29a79f0bc41d7c9f504849d3e1a1800d0d5a2a6b
asdf99245 <asdf99245@naver.com>
2022-12-05 20:29:14 +0900

    refactor: head태그 내 공통 내용은 document.tsx에서 정의하게 변경

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,8 +1,8 @@
-import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
@@ -26,10 +26,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta
-            name='description'
-            content='지루할 수 있는 Github 활동을 게임과 유사한 등급 시스템을 통해 조금 더 재미있게 할 수 있게 유도하기 위해 만들어진 서비스입니다.'
-          />
+          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
           <meta name='robots' content='index, follow' />
           <link rel='icon' href='/favicon.ico' />
           <link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css' />
