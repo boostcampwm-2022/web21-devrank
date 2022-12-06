ae3f56b0f5ee04d0e4e84d593c689271434e4735
asdf99245 <asdf99245@naver.com>
2022-12-06 17:34:10 +0900

    feat: Piechart 컴포넌트 작성

---
+++ frontend/src/components/Chart/PieChart.tsx
@@ -0,0 +1,105 @@
+import { ResponsivePie } from '@nivo/pie';
+
+function PieChart(data: unknown[]) {
+  return (
+    <ResponsivePie
+      data={data}
+      margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+      innerRadius={0.5}
+      padAngle={0.7}
+      cornerRadius={3}
+      activeOuterRadiusOffset={8}
+      borderWidth={1}
+      borderColor={{
+        from: 'color',
+        modifiers: [['darker', 0.2]],
+      }}
+      arcLinkLabelsSkipAngle={10}
+      arcLinkLabelsTextColor='#333333'
+      arcLinkLabelsThickness={2}
+      arcLinkLabelsColor={{ from: 'color' }}
+      arcLabelsSkipAngle={10}
+      arcLabelsTextColor={{
+        from: 'color',
+        modifiers: [['darker', 2]],
+      }}
+      fill={[
+        {
+          match: {
+            id: 'ruby',
+          },
+          id: 'dots',
+        },
+        {
+          match: {
+            id: 'c',
+          },
+          id: 'dots',
+        },
+        {
+          match: {
+            id: 'go',
+          },
+          id: 'dots',
+        },
+        {
+          match: {
+            id: 'python',
+          },
+          id: 'dots',
+        },
+        {
+          match: {
+            id: 'scala',
+          },
+          id: 'lines',
+        },
+        {
+          match: {
+            id: 'lisp',
+          },
+          id: 'lines',
+        },
+        {
+          match: {
+            id: 'elixir',
+          },
+          id: 'lines',
+        },
+        {
+          match: {
+            id: 'javascript',
+          },
+          id: 'lines',
+        },
+      ]}
+      legends={[
+        {
+          anchor: 'bottom',
+          direction: 'row',
+          justify: false,
+          translateX: 0,
+          translateY: 56,
+          itemsSpacing: 0,
+          itemWidth: 100,
+          itemHeight: 18,
+          itemTextColor: '#999',
+          itemDirection: 'left-to-right',
+          itemOpacity: 1,
+          symbolSize: 18,
+          symbolShape: 'circle',
+          effects: [
+            {
+              on: 'hover',
+              style: {
+                itemTextColor: '#000',
+              },
+            },
+          ],
+        },
+      ]}
+    />
+  );
+}
+
+export default PieChart;
