db7bfe5baf562cf562dd0e7f1fd2a63a2429a469
tunggary <gsy1656@gmail.com>
2022-12-07 12:11:55 +0900

    test: EXPbar 스토리북 수정

--- frontend/src/components/Profile/EXPbar/EXPbar.stories.tsx
+++ frontend/src/components/Profile/EXPbar/EXPbar.stories.tsx
@@ -4,20 +4,62 @@ import EXPbar from '@components/Profile/EXPbar';
 export default {
   title: 'Components/EXPbar',
   component: EXPbar,
-  argTypes: {
-    exp: {
-      control: {
-        type: 'range',
-        min: 0,
-        max: 699,
-      },
-    },
-  },
 } as ComponentMeta<typeof EXPbar>;
 
 const Template: ComponentStory<typeof EXPbar> = (args) => <EXPbar {...args} />;
 
-export const Basic = Template.bind({});
-Basic.args = {
-  exp: 90,
+export const Yellow = Template.bind({});
+Yellow.args = {
+  tier: 'yellow',
+  exp: 99,
+  needExp: 1,
+  startExp: 0,
+};
+
+export const Green = Template.bind({});
+Green.args = {
+  tier: 'green',
+  exp: 101,
+  needExp: 98,
+  startExp: 100,
+};
+
+export const Mint = Template.bind({});
+Mint.args = {
+  tier: 'mint',
+  exp: 250,
+  needExp: 249,
+  startExp: 200,
+};
+
+export const Blue = Template.bind({});
+Blue.args = {
+  tier: 'blue',
+  exp: 999,
+  needExp: 1,
+  startExp: 500,
+};
+
+export const Purple = Template.bind({});
+Purple.args = {
+  tier: 'purple',
+  exp: 1500,
+  needExp: 499,
+  startExp: 1000,
+};
+
+export const Orange = Template.bind({});
+Orange.args = {
+  tier: 'orange',
+  exp: 2001,
+  needExp: 2998,
+  startExp: 2000,
+};
+
+export const Red = Template.bind({});
+Red.args = {
+  tier: 'red',
+  exp: 6000,
+  needExp: 0,
+  startExp: 5000,
 };
