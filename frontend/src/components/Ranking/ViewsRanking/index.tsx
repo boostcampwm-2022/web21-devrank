5bf30f4742235af9f1f1704e08962be01b9b5252
tunggary <gsy1656@gmail.com>
2022-12-07 17:42:14 +0900

    refactor: ViewsRanking 컴포넌트 추가
    Co-authored-by: asdf99245 <asdf99245@users.noreply.github.com>

---
+++ frontend/src/components/Ranking/ViewsRanking/index.tsx
@@ -0,0 +1,54 @@
+import { useTranslation } from 'next-i18next';
+import { useQuery } from '@tanstack/react-query';
+import { RankingResponse } from '@type/response';
+import { RankingTable } from '@components/Ranking';
+import { Avatar, CubeIcon } from '@components/common';
+import { requestTopRankingByViews } from '@apis/ranking';
+
+interface ViewsRankingProps {
+  searchUser: (username: string) => void;
+}
+
+function ViewsRanking({ searchUser }: ViewsRankingProps) {
+  const { data: rankingByViews } = useQuery<RankingResponse[]>(['top-ranking-by-views'], () =>
+    requestTopRankingByViews(),
+  );
+
+  const { t } = useTranslation(['index', 'common']);
+
+  return (
+    <>
+      <h3>{t('index:daily-views-top-3')}</h3>
+      <RankingTable
+        width={'512px'}
+        columnWidthList={['12%', '8%', '58%', '20%']}
+        columnAlignList={['left', 'left', 'left', 'right']}
+      >
+        <RankingTable.Head>
+          <RankingTable.Element>{t('common:table-tier')}</RankingTable.Element>
+          <RankingTable.Element>#</RankingTable.Element>
+          <RankingTable.Element>{t('common:table-user')}</RankingTable.Element>
+          <RankingTable.Element>{t('common:table-views')}</RankingTable.Element>
+        </RankingTable.Head>
+        {rankingByViews?.map(({ id, tier, username, avatarUrl, dailyViews }, index) => (
+          <RankingTable.Row key={id} onClick={() => searchUser(username)}>
+            <RankingTable.Element>
+              <CubeIcon tier={tier} />
+            </RankingTable.Element>
+            <RankingTable.Element>
+              <span>{index + 1}</span>
+            </RankingTable.Element>
+            <RankingTable.Element>
+              <Avatar src={avatarUrl} name={username} />
+            </RankingTable.Element>
+            <RankingTable.Element>
+              <span>{dailyViews.toLocaleString()}</span>
+            </RankingTable.Element>
+          </RankingTable.Row>
+        ))}
+      </RankingTable>
+    </>
+  );
+}
+
+export default ViewsRanking;
