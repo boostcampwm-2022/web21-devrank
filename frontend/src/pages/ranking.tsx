5ea66c25785bf3008f7501bb4b27e04d6e03cb0f
asdf99245 <asdf99245@naver.com>
2022-12-15 16:52:49 +0900

    refactor: 랭킹페이지 검색 텍스트i18next interpolation 적용

--- frontend/src/pages/ranking.tsx
+++ frontend/src/pages/ranking.tsx
@@ -55,11 +55,7 @@ function Ranking({ tier, username, page }: RankingProps) {
       <Container>
         <Filterbar active={tier} setActive={setTier} />
         <SearchbarContainer>
-          {username !== '' && (
-            <SearchInfo>
-              {router.locale === 'ko' ? `"${username}"${t('search-user')}` : `${t('search-user')} "${username}"`}
-            </SearchInfo>
-          )}
+          {username !== '' && <SearchInfo>{t('ranking:search-user', { username })}</SearchInfo>}
           <RankingSearchbar placeholder={t('ranking:search-placeholder')} width={200} onSearch={setUsername} />
         </SearchbarContainer>
         <RankingTable
