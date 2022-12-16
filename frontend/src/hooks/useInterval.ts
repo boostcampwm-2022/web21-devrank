128c10a36a93519e16ab89ccfabf0f72251e60b6
tunggary <gsy1656@gmail.com>
2022-12-16 11:50:06 +0900

    refactor: useInterval 수정

--- frontend/src/hooks/useInterval.ts
+++ frontend/src/hooks/useInterval.ts
@@ -1,31 +1,17 @@
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 
 interface CallbackType {
   (): void;
 }
 
 export function useInterval(callback: CallbackType, delay: number, isActivate: boolean) {
-  const savedCallback = useRef<CallbackType>();
-
-  useEffect(() => {
-    if (callback) {
-      savedCallback.current = callback;
-    }
-  }, [callback]);
-
   useEffect(() => {
     if (!isActivate) return;
 
-    function tick() {
-      if (savedCallback.current) {
-        savedCallback.current();
-      }
-    }
-
-    const id = setInterval(tick, delay);
+    const id = setInterval(callback, delay);
 
     return () => {
       clearInterval(id);
     };
-  }, [delay, isActivate]);
+  }, [callback, delay, isActivate]);
 }
